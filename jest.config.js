1b47f0f28cd56da708d5da2ebd30cb8f0af0728f
William Andreu <willy14mx@gmail.com>
2024-07-26 07:11:47 +0200

    Changed ng test runner configuration

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   preset: 'jest-preset-angular',
   testEnvironment: "jsdom",
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  //setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   moduleNameMapper: {
     '@app/(.*)': '<rootDir>/src/app/$1',
     '@environments/(.*)': '<rootDir>/src/environments/$1',
